645bb4c73fbddc1eeb071e083b0b6b7a61753f2b
Szymon Chwila <Szymon_Chwila@macbook-pro-bozena.home>
2022-01-20 19:58:03 +0100

    added styles to ArtcilePage

--- src/components/shared/ArticlesList/ArticlesList.js
+++ src/components/shared/ArticlesList/ArticlesList.js
@@ -1,12 +1,26 @@
+import ArticlePage from '../../../pages/ArticlePage/ArticlePage';
 import ArticleItem from '../ArticleItem/ArticleItem';
+import { Link } from 'react-router-dom';
 import './ArticlesList.scss';
 
 function ArticlesList() {
   return (
     <div className='article-list-container'>
-      <ArticleItem></ArticleItem>
-      <ArticleItem></ArticleItem>
-      <ArticleItem></ArticleItem>
+      <p className='news-title'>Aktualności</p>
+      <div className='articles-container'>
+        <Link to={'/article/'}>
+          <ArticleItem></ArticleItem>
+        </Link>
+        <Link to={'/article/'}>
+          <ArticleItem></ArticleItem>
+        </Link>
+        <Link to={'/article/'}>
+          <ArticleItem></ArticleItem>
+        </Link>
+      </div>
+      <div className='more-news-button-container'>
+        <button className='more-news-button'>Więcej aktualności</button>
+      </div>
     </div>
   );
 }
--- src/components/shared/SectionInfo/SectionInfo.js
+++ src/components/shared/SectionInfo/SectionInfo.js
@@ -18,7 +18,16 @@ function SectionInfo() {
       <p className='location'>
         Aliquam erat volutpat. <br /> Nunc id sem enim.
       </p>
-      <div className='goglemapa'></div>
+      <div className='google-maps-localisation'>
+        {/* <iframe
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d82049.00784139201!2d21.915878193645028!3d50.010764891975676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473cfb0487a9e705%3A0x45889be6cac7200a!2sRevolutionary%20Act%20Monument!5e0!3m2!1sen!2spl!4v1642455110754!5m2!1sen!2spl'
+          width='300'
+          height='100'
+          style='border:0;'
+          allowfullscreen
+          loading='lazy'
+        ></iframe> */}
+      </div>
       <h2 className='enrolments'>zapisy</h2>
       <p className='phone-nr'>nr tel.: 000 000 000</p>
       <ContactForm></ContactForm>
