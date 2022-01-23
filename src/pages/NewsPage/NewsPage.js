85c06f70ad7d010e15be4793e49bbd3478230aef
Jakub Pisula <jakubpisula@macbook-pro-jakub-2.home>
2022-01-23 18:58:22 +0100

    Added propotype of news list page, added hover to article item

--- src/pages/NewsPage/NewsPage.js
+++ src/pages/NewsPage/NewsPage.js
@@ -6,7 +6,7 @@ function NewsPage() {
     <div className='news-page-content'>
       <section className='news-list'>
         <div className='container'>
-          <h2 className='news-h2'>Aktualności</h2>
+          <h1 className='news-h2'>Aktualności</h1>
           <ArticlesList
             className='articles-list'
             numberOfItems={window.innerWidth > 1440 ? 12 : 10}
