328cab098009f03154a6cf82fa9ae8bd171c89fe
Jakub Pisula <jakubpisula@macbook-pro-jakub-2.home>
2022-01-23 15:11:18 +0100

    ArticlePage Refactor - styles for mobile done

--- src/pages/ArticlePage/ArticlePage.js
+++ src/pages/ArticlePage/ArticlePage.js
@@ -4,10 +4,12 @@ import examImg from './assets/exam.jpeg';
 import { articles } from '../../database/articles/articles';
 import { BsFacebook, BsWhatsapp } from 'react-icons/bs';
 import { FaFacebookMessenger } from 'react-icons/fa';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import Button from '../../components/shared/Button/Button';
 
 function ArticlePage() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const article = id ? articles[id] : articles[0];
   return (
     <section className='article-page'>
@@ -15,8 +17,8 @@ function ArticlePage() {
         <div className='article-page-grid-container'>
           <article className='article-container'>
             <header>
-              <h2 className='article-title'>{article.title}</h2>
-              <div className='date-and-share-container'>
+              <h1>{article.title}</h1>
+              <section className='date-and-share-container'>
                 <p className='article-date'>
                   dodano: {new Date(article.date).toDateString()}
                 </p>
@@ -25,7 +27,7 @@ function ArticlePage() {
                   <FaFacebookMessenger className='media-icon' />
                   <BsWhatsapp className='media-icon' />
                 </div>
-              </div>
+              </section>
               <img
                 src={article.imageUrl}
                 alt='article-photo'
@@ -42,16 +44,14 @@ function ArticlePage() {
 
           <section className='article-list-container'>
             <header className='news-title-container'>
-              <p className='news-title'>Aktualności</p>
+              <h2 className='news-h2'>Aktualności</h2>
             </header>
             <main>
-              <ArticlesList
-                className='article-list-container'
-                numberOfItems={4}
+              <ArticlesList className='article-list' numberOfItems={4} />
+              <Button
+                text='Więcej aktualności'
+                onClick={() => navigate('/newslist', { replace: true })}
               />
-              <div className='more-news-button-container'>
-                <button className='more-news-button'>Więcej aktualności</button>
-              </div>
             </main>
           </section>
         </div>
