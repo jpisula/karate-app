2ab693f8bedbccc34ffdca3e785f1a04444cb91c
Jakub Pisula <jakubpisula@macbook-pro-jakub-2.home>
2022-01-31 14:09:05 +0100

    Added footer info

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,27 @@ import './Footer.scss';
 
 function Footer({ footerConfig }) {
   const { logo } = footerConfig;
-  return <footer>{/* <NavLogo logo={logo} /> */}</footer>;
+  return (
+    <footer className='footer'>
+      <section>
+        <NavLogo logo={logo} />
+        <div className='footer-info'>
+          <p>Michał Bodziony</p>
+          <p>tel. 000 000 000</p>
+        </div>
+      </section>
+      <div className='formal-info'>
+        <p className='creator-ad'>
+          This website was created in collaboration with{' '}
+          <span>Gancle Studio</span>{' '}
+        </p>
+        <p className='copyright'>
+          <small>&copy;</small> Copyright {new Date().getFullYear()},
+          oyama-karate.eu. All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header/NavLogo.js
+++ src/components/Header/NavLogo.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import './Header.scss';
+import './NavLogo.scss';
 
 const NavLogo = ({ logo }) => {
   const { title, titleHTML, src } = logo;
