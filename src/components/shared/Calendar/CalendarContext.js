d563f4b374a29dc64c43f203d1ef502bf7ad14d8
Szymon Chwila <Szymon_Chwila@macbook-pro-bozena.home>
2022-01-31 18:04:28 +0100

    changed all useStates to reducers

--- src/components/shared/Calendar/CalendarContext.js
+++ src/components/shared/Calendar/CalendarContext.js
@@ -4,12 +4,39 @@ import CalendarReducer from './CalendarReducer';
 const CalendarContext = createContext();
 
 export const CalendarProvider = ({ children }) => {
-  const initialState = { event: undefined };
+  const initialState = {
+    event: {},
+    chosenMonth: 0,
+    chosenYear: 2022,
+    louderForOthers: false,
+    startDay: 6,
+    isYearSwitcherOpen: false,
+    isMonthSwitcherOpen: false,
+    prevChosenYear: 2022,
+    prevChosenMonth: 0,
+    calendarTitle: `STYCZEŃ 2022`,
+    daysToMove: 0
+  };
 
   const [state, dispatch] = useReducer(CalendarReducer, initialState);
 
   return (
-    <CalendarContext.Provider value={{ event: state, dispatch }}>
+    <CalendarContext.Provider
+      value={{
+        event: state.event,
+        chosenYear: state.chosenYear,
+        chosenMonth: state.chosenMonth,
+        louderForOthers: state.louderForOthers,
+        startDay: state.startDay,
+        isYearSwitcherOpen: state.isYearSwitcherOpen,
+        isMonthSwitcherOpen: state.isMonthSwitcherOpen,
+        prevChosenYear: state.prevChosenYear,
+        prevChosenMonth: state.prevChosenMonth,
+        calendarTitle: state.calendarTitle,
+        daysToMove: state.daysToMove,
+        dispatch
+      }}
+    >
       {children}
     </CalendarContext.Provider>
   );
