bbae023a76909a2fef7b73f24a2cc051ebdb3b05
Jakub Pisula <jakubpisula@macbook-pro-jakub.home>
2022-02-01 13:23:39 +0100

    Added Expansible Accordion to the schedule page

---
+++ src/configs/trainings-schedule.js
@@ -0,0 +1,179 @@
+export const trainingsSchedule = [
+  {
+    name: 'Grupy Dziecięce (4-6 lat)',
+    sections: [
+      {
+        place: 'SP. Nr 61, ZSS Nr 12 Katowice-Ligota',
+        address: 'Ul. Kołobrzeska 8',
+        schedule: [
+          {
+            days: 'Wtorek',
+            hours: '16:30'
+          }
+        ],
+        instructor: 'Sensei Michał Bodziony',
+        instructorEmail: 'michalbodziony@oyama-karate.eu',
+        instructorPhone: '600-383-727',
+        instructorHelpers: []
+      },
+      {
+        place: 'MDK Południe Katowice - Podlesie',
+        address: 'Ul. Sołtysia 25',
+        schedule: [
+          {
+            days: 'Poniedziałek',
+            hours: '15:20'
+          }
+        ],
+        instructor: 'Sensei Michał Bodziony',
+        instructorEmail: 'michalbodziony@oyama-karate.eu',
+        instructorPhone: '600-383-727',
+        instructorHelpers: []
+      },
+      {
+        place: 'SP. Nr 28 Gliwice',
+        address: 'Ul. M. Strzody 4',
+        schedule: [
+          {
+            days: 'Piątek',
+            hours: '17:15'
+          }
+        ],
+        instructor: 'Sensei Michał Bodziony',
+        instructorEmail: 'michalbodziony@oyama-karate.eu',
+        instructorPhone: '600-383-727',
+        instructorHelpers: []
+      }
+    ]
+  },
+  {
+    name: 'Grupy Dzieci Szkolnych (7+ lat)',
+    sections: [
+      {
+        place: 'SP. Nr 35 Katowice - Ligota',
+        address: 'Ul. Zielonogórska 23',
+        schedule: [
+          {
+            days: 'Wtorek, Czwartek',
+            hours: '17:45'
+          }
+        ],
+        instructor: 'Sensei Michał Bodziony',
+        instructorEmail: 'michalbodziony@oyama-karate.eu',
+        instructorPhone: '600-383-727',
+        instructorHelpers: ['Senpai Dawid Kłyż']
+      },
+      {
+        place: 'SP. Nr 67 Katowice - Panewniki',
+        address: 'Ul. Zielona 5',
+        schedule: [
+          {
+            days: 'Poniedziałek',
+            hours: '18:00'
+          },
+          {
+            days: 'Środa',
+            hours: '17:15'
+          }
+        ],
+        instructor: 'Sensei Michał Bodziony',
+        instructorEmail: 'michalbodziony@oyama-karate.eu',
+        instructorPhone: '600-383-727',
+        instructorHelpers: []
+      },
+      {
+        place: 'MDK Południe Katowice - Podlesie',
+        address: 'Ul. Sołtysia 25',
+        schedule: [
+          {
+            days: 'Poniedziałek',
+            hours: '16:00'
+          }
+        ],
+        instructor: 'Sensei Michał Bodziony',
+        instructorEmail: 'michalbodziony@oyama-karate.eu',
+        instructorPhone: '600-383-727',
+        instructorHelpers: []
+      },
+      {
+        place: 'MDK Południe Katowice - Podlesie',
+        address: 'Ul. Sołtysia 25',
+        schedule: [
+          {
+            days: 'Poniedziałek',
+            hours: '16:00'
+          }
+        ],
+        instructor: 'Sensei Michał Bodziony',
+        instructorEmail: 'michalbodziony@oyama-karate.eu',
+        instructorPhone: '600-383-727',
+        instructorHelpers: []
+      },
+      {
+        place: 'SP. Nr 28 Gliwice - Centrum',
+        address: 'Ul. M. Strzody 4',
+        schedule: [
+          {
+            days: 'Wtorek, Piątek',
+            hours: '18:00'
+          }
+        ],
+        instructor: 'Sensei Michał Bodziony',
+        instructorEmail: 'michalbodziony@oyama-karate.eu',
+        instructorPhone: '600-383-727',
+        instructorHelpers: ['Senpai Tomasz Kos']
+      }
+    ]
+  },
+  {
+    name: 'Młodzież, Studenci i Dorośli',
+    sections: [
+      {
+        place: 'SP. Nr35 Katowice - Ligota',
+        address: 'Ul. Zielonogórska 23',
+        schedule: [
+          {
+            days: 'Wtorek, Czwartek',
+            hours: '18:45'
+          }
+        ],
+        instructor: 'Sensei Michał Bodziony',
+        instructorEmail: 'michalbodziony@oyama-karate.eu',
+        instructorPhone: '600-383-727',
+        instructorHelpers: ['Senpai Dawid Kłyż']
+      },
+      {
+        place: 'SP. Nr 67 Katowice - Panewniki',
+        address: 'Ul. Zielona 5',
+        schedule: [
+          {
+            days: 'Poniedziałek',
+            hours: '18:00'
+          },
+          {
+            days: 'Środa',
+            hours: '17:15'
+          }
+        ],
+        instructor: 'Sensei Michał Bodziony',
+        instructorEmail: 'michalbodziony@oyama-karate.eu',
+        instructorPhone: '600-383-727',
+        instructorHelpers: ['Senpai Dawid Kłyż']
+      },
+      {
+        place: 'SP. Nr 28 Gliwice - Centrum',
+        address: 'Ul. M. Strzody 4',
+        schedule: [
+          {
+            days: 'Wtorek, Piątek',
+            hours: '18:00'
+          }
+        ],
+        instructor: 'Sensei Michał Bodziony',
+        instructorEmail: 'michalbodziony@oyama-karate.eu',
+        instructorPhone: '600-383-727',
+        instructorHelpers: ['Senpai Tomasz Kos']
+      }
+    ]
+  }
+];
--- src/pages/info-pages/TrainingsSchedule/TrainingsSchedule.js
+++ src/pages/info-pages/TrainingsSchedule/TrainingsSchedule.js
@@ -1,43 +1,23 @@
 import './TrainingsSchedule.scss';
+import Collapsible from 'react-collapsible';
 import ArticleListContainer from '../../../components/shared/ArticleListContainer/ArticleListContainer';
+import { trainingsSchedule } from '../../../configs/trainings-schedule';
+import { BsChevronDown } from 'react-icons/bs';
 
-function TrainingsSchedule() {
-  const schedule = [
-    {
-      name: 'Grupy Dziecięce (4-6 lat)',
-      sections: [
-        {
-          place: 'SP. Nr 61, ZSS Nr 12 Katowice-Ligota',
-          address: 'Ul. Kołobrzeska 8',
-          days: 'Wtorek',
-          hours: '16:30',
-          instructor: 'Sensei Michał Bodziony',
-          instructorEmail: 'michalbodziony@oyama-karate.eu',
-          instructorPhone: '600-383-727'
-        },
-        {
-          place: 'MDK Południe Katowice - Podlesie',
-          address: 'Ul. Sołtysia 25',
-          days: 'Poniedziałek 15:20',
-          instructor: 'Sensei Michał Bodziony',
-          instructorEmail: 'michalbodziony@oyama-karate.eu',
-          instructorPhone: '600-383-727'
-        },
-        {
-          place: 'SP. Nr 28 Gliwice',
-          address: 'Ul. M. Strzody 4',
-          days: 'Piątek 17:15',
-          instructor: 'Sensei Michał Bodziony',
-          instructorEmail: 'michalbodziony@oyama-karate.eu',
-          instructorPhone: '600-383-727'
-        }
-      ]
-    },
-    {
-      name: 'Grupy Dzieci Szkolnych Dzieci od lat 7',
-      sections: [{}]
+const TrainingsSchedule = () => {
+  const generateCollapsibleElements = () => {
+    const schedule = [];
+    for (const scheduleGroup of trainingsSchedule) {
+      schedule.push(
+        <Collapsible
+          trigger={[<h3>{scheduleGroup.name}</h3>, <BsChevronDown />]}
+        >
+          <p>World</p>
+        </Collapsible>
+      );
     }
-  ];
+    return schedule;
+  };
 
   return (
     <section className='trainings-schedule'>
@@ -47,13 +27,18 @@ function TrainingsSchedule() {
             <h1>Harmonogram Zajęć</h1>
           </header>
           <main>
-            <table></table>
+            <div className='container'>
+              <h2>Wybierz grupę wiekową, która Cię interesuje: </h2>
+              <section className='schedule'>
+                {generateCollapsibleElements()}
+              </section>
+            </div>
           </main>
         </section>
         <ArticleListContainer />
       </div>
     </section>
   );
-}
+};
 
 export default TrainingsSchedule;
