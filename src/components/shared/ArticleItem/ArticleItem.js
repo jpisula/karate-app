94507b0c0e75f810c373c16e1161e9cabe3ddfeb
Jakub Pisula <jakubpisula@macbook-pro-jakub-2.home>
2022-02-02 10:38:02 +0100

    Article Item: Made image a link

--- src/components/shared/ArticleItem/ArticleItem.js
+++ src/components/shared/ArticleItem/ArticleItem.js
@@ -4,11 +4,13 @@ import './ArticleItem.scss';
 function ArticleItem({ article, id }) {
   return (
     <article className='article-item-container'>
-      <img
-        src={article.imageUrl}
-        alt='Article image'
-        className='article-item-image'
-      />
+      <Link to={`/article/${id}`}>
+        <img
+          src={article.imageUrl}
+          alt='Article image'
+          className='article-item-image'
+        />
+      </Link>
       <h2 className='article-item-title'>
         <Link to={`/article/${id}`}>{article.title}</Link>
       </h2>
