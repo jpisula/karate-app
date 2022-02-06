ee84e99102d3d0648a11bc968eb418a79d8853eb
Jakub Pisula <jakubpisula@macbook-pro-jakub.home>
2022-02-06 20:36:46 +0100

    Fix for font sizes, added full about oyama karate page

--- src/configs/aboutOyama.js
+++ src/configs/aboutOyama.js
@@ -25,11 +25,12 @@ export const aboutOyama = (
       znakomitej pozycji "Perfect Karate".
     </p>
     <br />
-    <img
-      src='http://oyama-karate.pl/_d/soshu_shigeru_oyama.jpg'
-      alt='Soshu Shigeru Oyama'
-      className='left-img'
-    />
+    <div className='left-img'>
+      <img
+        src='http://oyama-karate.pl/_d/soshu_shigeru_oyama.jpg'
+        alt='Soshu Shigeru Oyama'
+      />
+    </div>
     <br />
     <p>
       Styl OYAMA jest rozwijany pod kątem sztuki i samoobrony oraz w kontaktowej
@@ -69,5 +70,150 @@ export const aboutOyama = (
       Źródło:{' '}
       <a href='https://www.oyama-karate.pl/'>https://www.oyama-karate.pl/</a>{' '}
     </p>
+    <br />
+    <br />
+    <h2>Hanshi Jan Dyduch, 8 Dan</h2>
+    <p style={{ fontStyle: 'italic' }}>
+      "W Oyama Karate nikt nie pyta się o religię, narodowość czy kraj. Wszyscy
+      tworzymy jedną wielką rodzinę, wszyscy jesteśmy ludźmi karate. Bardzo
+      lubię przyjeżdżać do Polski./.../ Podczas kolejnych wizyt dostrzegłem jak
+      wiele zmienia się w waszym kraju. Bądźcie z siebie dumni i kochajcie swoją
+      ojczyznę. Uczcie się, pracujcie i trenujcie solidnie.Teraz macie to
+      wszystko, co na zachodzie. Bijecie Amerykanów, Japończyków i Europę i chcę
+      żebyście dalej tak walczyli. Jesteście szczęściarzami, macie znakomitego
+      mistrza i lidera, jakim jest sensei Jan Dyduch. Idźcie za nim..."
+    </p>
+    <p style={{ width: '100%', textAlign: 'right' }}>
+      Wielki Mistrz Shigeru Oyama.
+    </p>
+    <br />
+    <div className='left-img'>
+      <img
+        src='https://www.oyama-karate.pl/_d/static/hanshi_jan_dyduch.jpg'
+        alt='Hanshi Jan Dyduch'
+      />
+    </div>
+    <br />
+    <p>
+      Założycielem i liderem polskiej organizacji Oyama karate jest Hanshi Jan
+      Dyduch 8 dan z Krakowa (ur.1956 r.). Karate zaczął uprawiać od 1974 roku w
+      Ognisku Specjalistycznym TKKF "Karate-do Kyokushinkai" a potem w
+      Krakowskim Klubie Karate Kyokushinkai. W 1975 roku jako Reprezentant
+      Polski wziął udział w I Otwartych Mistrzostwach Świata Karate Kyokushin w
+      Japonii. Za swoją postawę otrzymał w Tokio od Kancho Masutatsu Oyamy
+      nagrodę specjalną. Rok później został Mistrzem Polski Karate Kyokushin w
+      kategorii ciężkiej i zdobył złoty medal w Międzynarodowym Turnieju Karate
+      Kyokushinkai "O Puchar Wawelskiego Smoka".
+    </p>
+    <br />
+    <p>
+      W 1977 roku, po powrocie z Indii i Afganistanu zakończył karierę
+      zawodniczą i rozpoczął działalność szkoleniową w Krakowie. W 1980 roku
+      zdał z wyróżnieniem egzamin na stopień mistrzowski 1 dan a w 1983 roku na
+      2 dan kyokushin karate. Oprócz kariery sportowej pełnił także wysokie
+      funkcje organizacyjne w polskim Kyokushin będąc m.in. Przewodniczącym
+      Komisji Kyokushinkai Polskiego Związku Karate i Prezesem Krakowskiego
+      Klubu Karate Kyokushinkai.
+    </p>
+    <br />
+    <div className='right-img'>
+      <img
+        src='https://www.oyama-karate.pl/_d/static/tokyo75.jpg'
+        alt='Hanshi Tournament'
+      />
+    </div>
+    <br />
+    <p>
+      W latach 1985-86 oraz w 1990 roku trenował w Stanach Zjednoczonych. W 1991
+      roku nawiązał kontakt z Wielkim Mistrzem Shigeru Oyamą 10 dan i w ślad za
+      nim, opuścił szeregi IKO kyokushin. Od 1991 roku stał się animatorem
+      rozwoju stylu OYAMA w Polsce. W 1992 roku zdał w Holandii test
+      weryfikacyjny na 2 dan a następnie egzamin na stopień mistrzowski 3 dan w
+      Oyama karate. W 1994 roku zdał egzamin na 4 dan. W 1997 roku został
+      promowany na stopień mistrzowski 5 dan i otrzymał tytuł Shihan. W dowód
+      uznania dla wysokich umiejętności technicznych i szkoleniowych, jak
+      również sukcesów organizacyjnych oraz sportowych w Polsce i na arenie
+      międzynarodowej, Shihan Jan Dyduch w 2001 roku, otrzymał z rąk Wielkiego
+      Mistrza Shigeru Oyamy 10 dan, nominację na stopień mistrzowski 6 dan, a w
+      2004 roku na 7 dan. Shihan Jan Dyduch wychował wielu znakomitych
+      zawodników i instruktorów m.in. mistrzów świata knockdown Oyama karate,
+      wielu medalistów mistrzostw Europy, dziesiątki medalistów mistrzostw
+      Polski i Pucharu Polski. Wyszkolił bądź wypromował blisko 270 posiadaczy
+      czarnych pasów Oyama karate w Polsce.
+    </p>
+    <br />
+    <p>
+      W 2003 r. liderzy współpracujących ze sobą ogólnopolskich związków karate
+      (Shorin-ryu, IFK Kyokushin, All Style Karate, Shotokan, Goju-ryu)
+      powierzyli mu funkcję prezesa Polskiej Federacji Karate.
+    </p>
+    <br />
+    <p>
+      W końcu 2004 roku Shihan Jan Dyduch został dyrektorem OYAMA
+      Międzynarodowej Federacji Karate (ang. OYAMA International Karate
+      Federation), działającej pod patronatem Soshu Shigeru Oyamy 10 dan. W
+      styczniu 2005 roku Światowa Organizacja Karate Kanreikai (ang. World
+      Karate Kanreikai), zajmująca się promocja karate sportowego na arenie
+      międzynarodowej, powierzyła mu funkcję dyrektora ds. europejskich. W 2006
+      roku został wybrany Prezesem Zarządu OYAMA Polskiej Federacji Karate i
+      Krakowskiego Klubu OYAMA.
+    </p>
+    <br />
+    <br />
+    <p>
+      Źródło:{' '}
+      <a href='https://www.oyama-karate.pl/'>https://www.oyama-karate.pl/</a>{' '}
+    </p>
+    <br />
+    <br />
+    <h2>Oyama Karate w Polsce</h2>
+    <p>
+      Początek rozwoju Oyama Karate w Polsce związany jest z 1991 rokiem, kiedy
+      to Jan Dyduch w imieniu grupy czołowych instruktorów i zawodników z
+      Krakowa złożył na ręce Wielkiego Mistrza Shigeru Oyamy deklarację
+      wstąpienia do Światowej Organizacji Oyama Karate (WOKO).
+    </p>
+    <br />
+    <p>
+      Styl OYAMA jako dyscyplina sportowa został zalegalizowany w Polsce w 1992
+      r. W tym samym roku zostało zarejestrowane Stowarzyszenie Oyama Karate w
+      Polsce z siedzibą w Krakowie. W 1993 roku powołany został do życia związek
+      sportowy, obecnie OYAMA Polska Federacja Karate (OYAMA PFK). Federacja
+      skupia aktualnie przeszło 50 klubów, w których trenuje około 6000 osób,
+      pod okiem około 220 licencjonowanych trenerów i instruktorów. OYAMA PFK
+      systematycznie szkoli instruktorów, sędziów i zawodników. W latach
+      1992-2007 przeszło 270 Polaków otrzymało z nadania Wielkiego Mistrza
+      Shigeru Oyamę 10 dan około 420 nominacji na różne stopnie mistrzowskie.
+      Około 50 sędziów ukończyło dwustopniowe specjalistyczne kursy. Co roku w
+      kraju lub zagranicą, organizowane są liczne letnie i zimowe seminaria oraz
+      obozy sportowo-szkoleniowe. Polska organizacja Oyama karate stale rozwija
+      od 1996 roku rywalizację sportową, w której biorą udział dzieci, juniorzy
+      i seniorzy obojga płci. Rozgrywane są ogólnopolskie zawody o randze
+      mistrzostw Polski i Pucharu Polski oraz liczne turnieje regionalne.
+      Najlepsi polscy zawodnicy startują w międzynarodowych zawodach sportowych
+      z mistrzostwami świata i Europy włącznie. Od 2003 roku, kluby OYAMA są
+      członkami Polskiej Federacji Karate.
+    </p>
+    <br />
+    <p>
+      Po tym jak Soshu Shigeru Oyama zamknął Centralne Dojo w Nowym Jorku i
+      przestał pełnić funkcję dyrektora World Oyama Karate Organization, polska
+      organizacja Oyama karate zrezygnowała z przynależności do WOKO i
+      przystąpiła do OYAMA Międzynarodowej Federacji Karate (ang. OYAMA
+      International Karate Federation), działającej pod jego patronatem. W 2005
+      roku polskie kluby przystąpiły także do Światowej Organizacji Kanreikai
+      Karate (ang. World Kanreikai Karate), zajmującej się promocją sportowego
+      karate na arenie międzynarodowej.
+    </p>
+    <br />
+    <p>OYAMA PFK wydaje liczne materiały szkoleniowe oraz informatory.</p>
+    <br />
+    <br />
+    <p>
+      Źródło:{' '}
+      <a href='https://www.oyama-karate.pl/'>https://www.oyama-karate.pl/</a>{' '}
+    </p>
+    <br />
+    <br />
   </>
 );
